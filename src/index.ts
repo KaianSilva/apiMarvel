e9a24ba6f49f0d4a57a63c1e869c15b630eea2c3
Kaian <kaian.f.silva@gmail.com>
2021-11-17 20:09:09 -0300

    include route person ID

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ const privateKey = process.env.PRIVATE_KEY;
 const publicKey = process.env.PUBLIC_KEY;
 const apiUrl = 'http://gateway.marvel.com/v1/public'
 
-const app = express();
+const app = express()
 
 const port = process.env.PORT
 
@@ -56,5 +56,41 @@ app.get('/personagem', (req: Request, res: Response, next: NextFunction) => {
 })
 
 app.get('/personagem/:id', (req: Request, res: Response, next: NextFunction) => {
+
+  const page:number = Number(req.query.page) - 1;
+  const limit:number = Number(req.query.limit);
+  const idPer:number = Number(req.params.id)
+  const ts = new Date().getTime().toString();
+  const hash = md5(ts + privateKey + publicKey);
+
+  axios.get(`${apiUrl}/characters`, {
+    params: {
+      ts: ts,
+      apikey: publicKey,
+      hash: hash,
+      id: idPer
+      /* orderBy: 'name',
+      limit: limit,
+      offset: page * limit */
+    }
+  }).then((response => {
+    const personagens: Array<any> = response.data.data.results;
+    const nomes: Array<any> = personagens.map(personagem => {
+      return {
+        nome: personagem.name,
+        id: personagem.id
+      }
+    });
+    const objRetorno = {
+      page: page + 1,
+      count: nomes.length,
+      totalPages: Math.floor((response.data.data.total/limit) + 1),
+      personagens: [...nomes]
+    };
+    res.json(objRetorno);
+  })).catch( err => {
+    console.log(err);
+    res.status(500).send("Internal error");
+  })
 
 })
