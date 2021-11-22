b8b6984b28430d20b1f64ad5fa9f50b06e054586
Kaian <kaian.f.silva@gmail.com>
2021-11-22 17:09:31 -0300

    get comics

--- src/index.ts
+++ src/index.ts
@@ -99,3 +99,44 @@ app.get('/personagem/:id', (req: Request, res: Response, next: NextFunction) => 
   })
 
 })
+
+
+
+app.get('/quadrinhos', (req: Request, res: Response, next: NextFunction) => {
+  const page:number = Number(req.query.page) - 1;
+  const limit:number = Number(req.query.limit);
+  const idChar:number = Number(req.query.character);
+  const ts = new Date().getTime().toString();
+  const hash = md5(ts + privateKey + publicKey);
+
+  axios.get(`${apiUrl}/comics`, {
+    params: {
+      ts: ts,
+      apikey: publicKey,
+      hash: hash,
+      characters: idChar,
+      limit: limit,
+      offset: page * limit
+    }
+  }).then((response => {
+    const quadrinhos: Array<any> = response.data.data.results;
+    const nomes: Array<any> = quadrinhos.map(quadrinhos => {
+      return   {
+        nome: quadrinhos.title,
+        descricao: quadrinhos.description,
+        id: quadrinhos.id,
+        thumbnail: quadrinhos.thumbnail
+      } 
+    });
+    const objRetorno = {
+      page: page + 1,
+      count: nomes.length,
+      totalPages: Math.floor((response.data.data.total/limit) + 1),
+      quadrinhos: [...nomes]
+    };
+    res.json(objRetorno);
+  })).catch( err => {
+    console.log(err);
+    res.status(500).send("Internal error");
+  })
+})
